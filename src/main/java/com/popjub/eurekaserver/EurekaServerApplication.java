823176a48d2ecf5674586fb9be207ce2e143949f
JungInGyu <dltnswn20@naver.com>
2025-12-04 16:07:53 +0900

    chore: package com.popjub 변경

--- src/main/java/com/popjub/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/popjub/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.sparta.eurekaserver;
+package com.popjub.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
